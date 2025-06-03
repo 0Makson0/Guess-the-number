66cb04376e343d8694c183f16dee396a071a992d
0Makson0 <maksimtumash1@gmail.com>
2025-06-04 00:02:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const button_Im = document.getElementById("button_Im");
+const button_Ai = document.getElementById("button_Ai");
+const inputContainer = document.getElementById("inputContainer")
+
+const removeButtonImAi = () => {
+	button_Im.style.display = "none";
+	button_Ai.style.display = "none";
+}
+
+const buttons = [button_Im, button_Ai];
+
+buttons.forEach(button => {
+    button.addEventListener("click", removeButtonImAi);
+});
+
+button_Im.addEventListener("click", () => {
+	const inputIm = document.createElement("input")
+	inputIm.type = "number";
+	inputIm.min = "1"
+	inputIm.mix = "100"
+	inputIm.placeholder = "Число від 1 до 100"
+	inputContainer.appendChild(inputIm)
+})
+
+const randomAi = function() {
+	return Math.floor(Math.random() * 100) + 1;
+}
